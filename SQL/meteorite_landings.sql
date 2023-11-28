691340a05991ec9d4a9839257cee9ce9812ded58
Arbash <144490515+arbash-malik@users.noreply.github.com>
2023-11-28 09:29:15 +0100

    Changed creation query and created dump for same

--- SQL/meteorite_landings.sql
+++ SQL/meteorite_landings.sql
@@ -5,21 +5,18 @@ USE term2;
 
 DROP TABLE IF EXISTS meteorite_landings;
 CREATE TABLE meteorite_landings (
-    name VARCHAR(45),
+    name VARCHAR(255),
     id INT NOT NULL,
-    nametype VARCHAR(45),
-    recclass VARCHAR(45),
-    mass_gramms VARCHAR(255),
-    mass_gramms_numeric FLOAT,
+    nametype VARCHAR(255) DEFAULT NULL,
+    recclass VARCHAR(255),
+    mass_g INT DEFAULT NULL,
     fall VARCHAR(255),
     year INT,
-    reclat FLOAT,
-    reclong FLOAT,
-    geolocation VARCHAR(45),
-    states VARCHAR(45),
-    counties VARCHAR(45),
-    PRIMARY KEY(id)
+    reclat DECIMAL(10,6) DEFAULT NULL,
+    reclong DECIMAL(10,6) DEFAULT NULL,
+    geolocation VARCHAR(255) DEFAULT NULL
 );
+
 
 -- load data into term2.meteorite_landings
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Meteorite_Landings.csv' 
@@ -28,16 +25,18 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
-(@name, @id, @nametype, @recclass, @mass_grams, @fall, @year, @reclat, @reclong, @geolocation)
+(@name, @id, @nametype, @recclass, @mass_g, @fall, @year, @reclat, @reclong, @geolocation)
 SET
-  name = @name,
+  name = nullif(@name, ''),
   id = @id,
-  nametype = @nametype,
-  recclass = @recclass,
-  mass_gramms = REPLACE(@mass_grams, ',', ''),
-  mass_gramms_numeric = NULLIF(REPLACE(@mass_grams, ',', ''), ''), -- Use NULLIF to handle empty strings
-  fall = @fall,
+  nametype = nullif(@nametype,''),
+  recclass = nullif(@recclass,''),
+  mass_g = nullif(@mass_g, ''),
+  fall = nullif(@fall,''),
   year = nullif(@year, ''),
   reclat = nullif(@reclat, ''),
   reclong = nullif(@reclong, ''),
   geolocation = nullif(@geolocation, '');
+  
+  
+  SELECT * FROM METEORITE_LANDINGS;
